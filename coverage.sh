ff05f7235dae731e58ceb2b86e3e4f528e0d4cfb
Josh Komoroske <jdkomo@gmail.com>
2018-10-12 20:43:21 -0700

    Remove Godel from build (#23)

---
+++ coverage.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+touch coverage.txt
+
+for pkg in $(go list ./... | grep -v vendor); do
+    go test -v -race -coverprofile=profile.out -covermode=atomic "$pkg"
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
